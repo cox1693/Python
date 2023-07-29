f4d2dd5b8171be2eb2cb6e55c06d41b5fa2d5b41
cox1693 <88863747+cox1693@users.noreply.github.com>
2023-07-28 22:32:12 -0400

    if statement

--- Hello.py
+++ Hello.py
@@ -1,5 +1,18 @@
 print ("Hello")
 print (5+3)
-a=b
+a=3
 b=5
 print (a+b)
+for  n in range(0,50):
+    print(n)
+    if n % 2 == 1:
+        print("odd")
+    else:
+        print("even")
+fruitlist =["apple", "orange", "grape", "banana"]
+for fruit in fruitlist:
+    print(fruit)
+c = 5 == 3
+d=7>3
+print (c)
+print (d)
